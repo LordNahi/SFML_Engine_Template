0086d076afb166ceaa6bd83cbb5982b081bad976
Nathan Nahi <nathannahisteam@gmail.com>
2021-01-03 22:24:42 -0800

    Added a float overload to randomRange

--- include/Util.hpp
+++ include/Util.hpp
@@ -3,4 +3,5 @@
 namespace Util::Math
 {
     int randRange(int min, int max);
+    float randRange(float min, float max);
 } // namespace Util::Math
--- src/Util.cpp
+++ src/Util.cpp
@@ -11,4 +11,14 @@ namespace Util::Math
 
         return distrib(gen);
     };
+
+    float randRange(float min, float max)
+    {
+        // Will this cause a memory leak if I call this func a million times ...
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> distrib(min, max);
+
+        return distrib(gen);
+    }
 } // namespace Util::Math
